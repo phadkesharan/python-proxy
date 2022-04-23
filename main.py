181e1174c74cd77e8f6bb36fb8df0a1baca855e3
Sharan Phadke <phadkesharan@gmail.com>
2022-04-23 12:47:35 +0530

    NEW: /packages/ endpoint

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from urllib import response
 from fastapi import FastAPI
 import requests
 from pydantic import BaseModel
@@ -9,6 +10,7 @@ LOGIN_ENDPOINT = API_ENDPOINT + "/api/login/"
 CSRF_ENDPOINT = API_ENDPOINT + "/api/csrf/"
 SITES_ENDPOINT = API_ENDPOINT + "/api/sites/"
 LOG_ENDPOINT = API_ENDPOINT + "/api/security/logs/"
+PACKAGE_ENDPOINT = API_ENDPOINT + "/api/packages/"
 
 class User(BaseModel):
     email: str
@@ -80,3 +82,24 @@ async def getSiteLog(user: User, site_url: str):
     print("cookies, ", res_login.cookies)
     res = requests.get(LOG_ENDPOINT + site_url + "/", cookies=res_login.cookies)
     print("response : ", res.json())
+
+    return res.json()
+
+
+@app.post("/packages/")
+async def packages(user: User):
+
+    LOGIN_CREDENTIALS = {
+            "email": user.email,
+            "password": user.password
+        }
+
+    res_login = requests.post(LOGIN_ENDPOINT, json=LOGIN_CREDENTIALS)
+    print("login reponse ", res_login.text)
+    print("cookies, ", res_login.cookies)
+    print("package endpoint", PACKAGE_ENDPOINT)
+    res = requests.get(PACKAGE_ENDPOINT, cookies=res_login.cookies)
+    print("response : ", res.json())
+
+    return res.json()
+    
